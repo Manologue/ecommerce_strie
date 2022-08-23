272430bade0a775175e9d23451a3b1ee005ea0bf
Moumi Manoah <manomoumi@gmail.com>
2022-08-23 12:56:36 +0100

    test on slug imges

--- components/Product.jsx
+++ components/Product.jsx
@@ -8,12 +8,14 @@ const Product = ({ product: { image, name, slug, price } }) => {
     <div className="product-container">
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
-          <img
-            src={urlFor(image && image[0])}
-            width={250}
-            height={250}
-            className="product-image"
-          />
+          {image[0] && (
+            <img
+              src={urlFor(image && image[0])}
+              width={250}
+              height={250}
+              className="product-image"
+            />
+          )}
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
         </div>
